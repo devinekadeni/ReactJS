02e41ca4fcbc5cdfe0813e27b7702775a0747c92
devinekadeni <devinekadeni@gmail.com>
2018-03-12 14:12:27 +0700

    updated redux promise

---
+++ WeatherForecast/src/actions/index.js
@@ -0,0 +1,1 @@
+const API_KEY = '94f3b67a9b69f4043e4d18835d21bed8';
--- WeatherForecast/src/containers/search_bar.js
+++ WeatherForecast/src/containers/search_bar.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            terms: ''
+        };
+    }
+
+    onInputChange(event) {
+        this.setState({terms: event.target.value});
+        console.log(this.state.terms);
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <form className="input-group" >
-                <input />
-                <span>
+            <form onSubmit={this.onFormSubmit} className="input-group" >
+                <input 
+                    value={this.state.terms} 
+                    onChange={this.onInputChange.bind(this)} //bind = supaya "this" global bisa dipake di dalem onInputChange
+                    placeholder="Get a Five-day forecast in your favorite cities"
+                    className="form-control" />
+                <span className="input-group-btn" >
                     <button type="submit" className="btn btn-secondary">Submit</button>
                 </span>
             </form>
