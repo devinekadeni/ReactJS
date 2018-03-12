52d88642772481a5a5cedef4ac9f17741153f5a3
devinekadeni <devinekadeni@gmail.com>
2018-03-12 15:47:15 +0700

    updated fetching weather

--- WeatherForecast/src/actions/index.js
+++ WeatherForecast/src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '94f3b67a9b69f4043e4d18835d21bed8';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},id`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
--- WeatherForecast/src/containers/search_bar.js
+++ WeatherForecast/src/containers/search_bar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
     constructor(props) {
         super(props);
 
@@ -16,11 +19,13 @@ export default class SearchBar extends Component {
 
     onFormSubmit(event) {
         event.preventDefault();
+        this.props.fetchWeather(this.state.terms);
+        this.setState({terms: ''});
     }
 
     render() {
         return (
-            <form onSubmit={this.onFormSubmit} className="input-group" >
+            <form onSubmit={this.onFormSubmit.bind(this)} className="input-group" >
                 <input 
                     value={this.state.terms} 
                     onChange={this.onInputChange.bind(this)} //bind = supaya "this" global bisa dipake di dalem onInputChange
@@ -33,3 +38,15 @@ export default class SearchBar extends Component {
         );
     }
 }
+
+function mapStateToProps(state) {
+    return {};
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({
+        fetchWeather
+    }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchBar);
