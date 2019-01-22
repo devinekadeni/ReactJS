67ee478e3c7d26eb164eef175abf6600241bbc46
Devin <devin@getcraft.com>
2019-01-23 01:16:48 +0700

    changed to class componnet

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import SearchBar from './SearchBar'
 
-const App = () => (
-  <div className="ui container" style={{ marginTop: '10px' }}>
-    <SearchBar />
-  </div>
-)
+class App extends Component {
+  state = {  }
+
+  onSearchSubmit = e => {
+    console.log('hello');
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    )
+  }
+}
 
 export default App
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -11,6 +11,7 @@ class SearchBar extends React.Component {
 
   onFormSubmit = (e) => {
     e.preventDefault()
+    this.props.onSubmit(this.state.term)
   }
 
   render() {
