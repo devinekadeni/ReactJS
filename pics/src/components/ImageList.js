15d7a6c298162bb3453a62e8223da6c490b5a31d
Devin <devin@getcraft.com>
2019-02-03 20:54:38 +0700

    added new component and style

---
+++ pics/src/components/ImageList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import ImageCard from './ImageCard'
+
+import './ImageList.css'
+
+const ImageList = props => (
+  <div className="image-list">
+    {props.images.map(image => (
+      <ImageCard key={image.id} image={image} />
+    ))}
+  </div>
+)
+
+export default ImageList
